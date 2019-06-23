39cde7c5176675dc9e7ad1ca6b397c45917e247f
David Michaux <michaux.dr@gmail.com>
2019-06-22 20:04:07 -0400

    rename controller to match

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'connect', to: 'pages#connect'
-  get 'portfolio', to: 'pages#portfolio'
+  root 'static_pages#home'
+  get 'about', to: 'static_pages#about'
+  get 'connect', to: 'static_pages#connect'
+  get 'portfolio', to: 'static_pages#portfolio'
 end
